fc9ca5428870669baadd9c1ccf2687a8104ce9a6
Andreyr76 <61867832+Andreyr76@users.noreply.github.com>
2020-11-21 14:18:38 +0300

    1

---
+++ my_arch_manjaro1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Arch Linux Fast Install - Быстрая установка Arch Linux https://github.com/ordanax/arch
+# Цель скрипта - быстрое развертывание системы с вашими персональными настройками (конфиг XFCE, темы, программы и т.д.).
+# Автор скрипта Алексей Бойко https://vk.com/ordanax
+
+
+loadkeys ru
+setfont cyr-sun16
+echo 'Скрипт сделан на основе чеклиста Бойко Алексея по Установке ArchLinux'
+echo 'Ссылка на чек лист есть в группе vk.com/arch4u'
+
+echo '2.3 Синхронизация системных часов'
+timedatectl set-ntp true
+
+
+
+echo '2.4.2 Форматирование дисков'
+
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4  /dev/sda2
+
+echo '2.4.3 Монтирование дисков'
+mount /dev/sda2 /mnt
+mkdir /mnt/home
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+mount /dev/sda3 /mnt/home
+
+echo '3.1 Выбор зеркал для загрузки.'
+rm -rf /etc/pacman.d/mirrorlist
+wget https://git.io/mirrorlist
+mv -f ~/mirrorlist /etc/pacman.d/mirrorlist
+
+echo '3.2 Установка основных пакетов'
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
+
+echo '3.3 Настройка системы'
+genfstab -pU /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/my_arch_manjaro2.sh)"
